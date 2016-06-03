5a41d44fcbc6d8afe7b1c97ec3ae724cd7220bda
Harianantha <hariharan.anantharaman@gmail.com>
2016-06-03 14:27:56 +0530

    changes to get values from propertu

--- ProductService/src/main/java/com/cognizant/microservices/demo/product/db/MongoDBUtil.java
+++ ProductService/src/main/java/com/cognizant/microservices/demo/product/db/MongoDBUtil.java
@@ -2,6 +2,8 @@ package com.cognizant.microservices.demo.product.db;
 
 import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
@@ -9,14 +11,40 @@ import com.mongodb.ServerAddress;
 
 public class MongoDBUtil {
 	
+	private static Map<String,String> variableMap=null;
+	
+	private static String MONGODB_SERVER_NAME="MONGODB_SERVER_NAME";
+	private static String MONGODB_SERVER_PORT="MONGODB_SERVER_PORT";
+	private static String MONGODB_DB_NAME="MONGODB_DB_NAME";
+	private static String MONGODB_DB_USERNAME="MONGODB_DB_USERNAME";
+	private static String MONGODB_DB_PASSWORD="MONGODB_DB_PASSWORD";
 	
 	public static MongoClient getMongoDBClient() throws UnknownHostException{
 		MongoClient mongoClient=null;
 		//mongoClient = new MongoClient( new ServerAddress("127.0.0.1",27017));
-		MongoCredential credential = MongoCredential.createCredential("harianantha", "samplecommerce", "mangal123".toCharArray());
+		if(variableMap==null){
+			populateEnvironmentVariableMap();
+			
+		}
+		System.out.println("Map is:"+variableMap);
+		MongoCredential credential = MongoCredential.createCredential(variableMap.get(MONGODB_DB_USERNAME),variableMap.get(MONGODB_DB_NAME), variableMap.get(MONGODB_DB_PASSWORD).toCharArray());
 
-		mongoClient = new MongoClient( new ServerAddress("ds019481.mlab.com",19481),Arrays.asList(credential));
-		return mongoClient;
+		mongoClient = new MongoClient( new ServerAddress(variableMap.get(MONGODB_SERVER_NAME),Integer.parseInt(variableMap.get(MONGODB_SERVER_PORT))),Arrays.asList(credential));
+			return mongoClient;
+	}
+	
+	private static void populateEnvironmentVariableMap(){
+		
+		if(variableMap==null){
+			variableMap=new HashMap<String,String>();
+			variableMap.put(MONGODB_SERVER_NAME, System.getProperty(MONGODB_SERVER_NAME)) ;
+			variableMap.put(MONGODB_SERVER_NAME, System.getProperty(MONGODB_SERVER_NAME)) ;
+			variableMap.put(MONGODB_SERVER_NAME, System.getProperty(MONGODB_SERVER_NAME)) ;
+			variableMap.put(MONGODB_SERVER_NAME, System.getProperty(MONGODB_SERVER_NAME)) ;
+			variableMap.put(MONGODB_SERVER_NAME, System.getProperty(MONGODB_SERVER_NAME)) ;
+		}
+		
+		
 	}
 
 }
